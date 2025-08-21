ccc15d44f12e81cc0f4b74aa2f57423bcafa0046
Turazy <guisoaresreis420@gmail.com>
2025-08-21 08:21:07 -0300

    Inicio

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <h1>Header</h1>
+   <header>
+    <h1>GV BIKES</h1>
+   <nav>
+    <Link to="/">Home</Link>
+    <Link to="/produtos">Produtos</Link>
+    <Link to="/categorias">Categorias</Link>
+    </nav>
+    </header>
   )
 }
--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -1,6 +1,52 @@
-
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import Header from '../components/Header'
+import axios from 'axios';
 export default function Categorias() {
+  const [categorias, setCategorias] = useState([]);
+  const [busca, setBusca] = useState('');
+  const api = axios.create({
+    baseURL: "http://localhost:3333"
+  })
+  useEffect(() => {
+    api.get("/categorias")
+      .then(res => setCategorias(res.data))
+      .catch(err => console.log("Erro ao buscar as categorias", err)
+      )
+  }, [api])
+
+  const categoriasFiltradas = busca.trim()
+    ? categorias.filter(categoria =>
+      categoria.name.toLowerCase()
+        .includes(busca.toLowerCase())
+    )
+    : categorias;
+
   return (
-    <h1>Categorias</h1>
+    <>
+      <Header />
+      <Link to="/cadastrar-categoria">
+        Adicionar uma categoria
+      </Link>
+      <input type="text"
+        placeholder='buscar categoria'
+        value={busca}
+        onChange={(e) => setBusca(e.target.value)}
+      />
+      {categoriasFiltradas.length === 0 ? (
+        <p>Nenhuma categoria foi cadastrada.</p>
+      ) : (
+        categoriasFiltradas.map(categoria => (
+          <div key={categoria.id}>
+            <p>{categoria.id}</p>
+            <p>{categoria.name}</p>
+            <p>{categoria.description}</p>
+            <Link to={`/editar-categoria/${categoria.id}`}>
+              Editar
+            </Link>
+          </div>
+        ))
+      )}
+    </>
   );
 }
